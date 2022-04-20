70638620e0b36cccfeeacebe0b01bd185bd83d27
Mariano Martin Battaglia <battaglia1902@gmail.com>
2022-04-20 20:40:50 -0300

    2da pre entrega. Implementacion de Local Storage y JSON.

--- productos.js
+++ productos.js
@@ -1,19 +1,20 @@
+/* // SECCION ELIMINADA DEBIDO A LA IMPLEMENTACION DE JSON
 function Producto(nombre, precio, cantidad) {
-    this.nombre  = nombre.toUpperCase();
+    this.nombre  = nombre;
     this.precio  = parseFloat(precio);
     this.cantidad = parseFloat(cantidad);
 }
 
-const producto1 = new Producto("Aros corazón Swarovski", 1850);
+const producto1 = new Producto("Aros Swarovski", 1850);
 const producto2 = new Producto("Cjto. corazón Cristal", 2350);
 const producto3 = new Producto("Argollitas con bolitas", 1990);
 const producto4 = new Producto("Anillo luna", 1250);
 const producto5 = new Producto("Aros corazón Blanco", 1850);
 const producto6 = new Producto("Conjunto ojo protector", 2150);
 
-// Change
+// Change - previo a JSON
 let input01  = document.getElementById("prod-name-1");
-input01.innerText = producto1.nombre
+input01.innerHTML = producto1.nombre
 let input02  = document.getElementById("prod-name-2");
 input02.innerText = producto2.nombre
 let input03  = document.getElementById("prod-name-3");
@@ -24,92 +25,104 @@ let input05  = document.getElementById("prod-name-5");
 input05.innerText = producto5.nombre
 let input06  = document.getElementById("prod-name-6");
 input06.innerText = producto6.nombre
+*/
 
+//JSON para Productos
+const productos = [{ id: 1,  producto: "Aros Swarovski", precio: 1850},
+                  {  id: 2,  producto: "Cjto. corazón Cristal", precio: 2350},
+                  {  id: 3,  producto: "Argollitas con bolitas"  , precio: 1990},
+                  {  id: 4,  producto: "Anillo luna" , precio: 1250},
+                  {  id: 5,  producto: "Aros corazón Blanco" , precio: 1850},
+                  {  id: 6,  producto: "Conjunto ojo protector" , precio: 2150}];
+
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+//Almacenar producto por producto
+for (const producto of productos) {
+    guardarLocal(producto.id, JSON.stringify(producto));
+}
+
+// Genero variables tomando los elementos JSON para luego utilizarlos en el DOM
+let item1 = localStorage.getItem('1')
+let item2 = localStorage.getItem('2')
+let item3 = localStorage.getItem('3')
+let item4 = localStorage.getItem('4')
+let item5 = localStorage.getItem('5')
+let item6 = localStorage.getItem('6')
+
+// Change
+let input01  = document.getElementById("prod-name-1");
+input01.innerHTML = JSON.parse(item1).producto;
+let input02  = document.getElementById("prod-name-2");
+input02.innerText = JSON.parse(item2).producto;
+let input03  = document.getElementById("prod-name-3");
+input03.innerText = JSON.parse(item3).producto;
+let input04  = document.getElementById("prod-name-4");
+input04.innerText = JSON.parse(item4).producto;
+let input05  = document.getElementById("prod-name-5");
+input05.innerText = JSON.parse(item5).producto;
+let input06  = document.getElementById("prod-name-6");
+input06.innerText = JSON.parse(item6).producto;
 
 // Eventos
 let padre = document.getElementById("carrito")
 
 // Boton 1
 let btn1 = document.getElementById("btn1");
-btn1.addEventListener("click", respuesta1);
-function respuesta1() {
-    //logica para guardar en el carrito
-    let lineaCarrito = document.createElement("li")
-    let nombreProducto = producto1.nombre
-    let precioProducto = producto1.precio
-    lineaCarrito.innerHTML = `${nombreProducto} x $${precioProducto}`
-    padre.appendChild(lineaCarrito)
-    alert(`Has agregado 1 item al carrito`)
-}
+btn1.onclick = () =>{ respuestaOk(btn1) }
 
 // Boton 2
 let btn2 = document.getElementById("btn2");
-btn2.addEventListener("click", respuesta2);
-function respuesta2() {
-    //logica para guardar en el carrito
-    let lineaCarrito = document.createElement("li")
-    let nombreProducto = producto2.nombre
-    let precioProducto = producto2.precio
-    lineaCarrito.innerHTML = `${nombreProducto} x $${precioProducto}`
-    padre.appendChild(lineaCarrito)
-    alert(`Has agregado 1 item al carrito`)
-}
+btn2.onclick = () =>{ respuestaOk(btn2) }
 
 // Boton 3
 let btn3 = document.getElementById("btn3");
-btn3.addEventListener("click", respuesta3);
-function respuesta3() {
-    //logica para guardar en el carrito
-    let lineaCarrito = document.createElement("li")
-    let nombreProducto = producto3.nombre
-    let precioProducto = producto3.precio
-    lineaCarrito.innerHTML = `${nombreProducto} x $${precioProducto}`
-    padre.appendChild(lineaCarrito)
-    alert(`Has agregado 1 item al carrito`)
-}
+btn3.onclick = () =>{ respuestaOk(btn3) }
 
 // Boton 4
 let btn4 = document.getElementById("btn4");
-btn4.addEventListener("click", respuesta4);
-function respuesta4() {
-    //logica para guardar en el carrito
-    let lineaCarrito = document.createElement("li")
-    let nombreProducto = producto4.nombre
-    let precioProducto = producto4.precio
-    lineaCarrito.innerHTML = `${nombreProducto} x $${precioProducto}`
-    padre.appendChild(lineaCarrito)
-    alert(`Has agregado 1 item al carrito`)
-}
+btn4.onclick = () =>{ respuestaOk(btn4) }
 
 // Boton 5
 let btn5 = document.getElementById("btn5");
-btn5.addEventListener("click", respuesta5);
-function respuesta5() {
-    //logica para guardar en el carrito
-    let lineaCarrito = document.createElement("li")
-    let nombreProducto = producto5.nombre
-    let precioProducto = producto5.precio
-    lineaCarrito.innerHTML = `${nombreProducto} x $${precioProducto}`
-    padre.appendChild(lineaCarrito)
-    alert(`Has agregado 1 item al carrito`)
-}
+btn5.onclick = () =>{ respuestaOk(btn5) }
 
 // Boton 6
 let btn6 = document.getElementById("btn6");
-btn6.addEventListener("click", respuesta6);
-function respuesta6() {
-    //logica para guardar en el carrito
-    let lineaCarrito = document.createElement("li")
-    let nombreProducto = producto6.nombre
-    let precioProducto = producto6.precio
-    lineaCarrito.innerHTML = `${nombreProducto} x $${precioProducto}`
-    padre.appendChild(lineaCarrito)
-    alert(`Has agregado 1 item al carrito`)
-}
+btn6.onclick = () =>{ respuestaOk(btn6) }
 
-// Funcion para agregar item a la lista/carrito
-function agregarCarrito(product) {
+function respuestaOk(id) {
     let lineaCarrito = document.createElement("li")
-    lineaCarrito.innerHTML = `${product.nombre} x $${product.precio}`
-    padre.appendChild(lineaCarrito)
+    let nombreProducto;
+    let precioProducto;
+    switch (id) {
+        case btn1: 
+            nombreProducto = JSON.parse(item1).producto;
+            precioProducto = JSON.parse(item1).precio;
+            break;
+        case btn2: 
+            nombreProducto = JSON.parse(item2).producto;
+            precioProducto = JSON.parse(item2).precio;
+            break;
+        case btn3: 
+            nombreProducto = JSON.parse(item3).producto;
+            precioProducto = JSON.parse(item3).precio;
+            break;
+        case btn4: 
+            nombreProducto = JSON.parse(item4).producto;
+            precioProducto = JSON.parse(item4).precio;
+            break;
+        case btn5: 
+            nombreProducto = JSON.parse(item5).producto;
+            precioProducto = JSON.parse(item5).precio;
+            break;
+        case btn6: 
+            nombreProducto = JSON.parse(item6).producto;
+            precioProducto = JSON.parse(item6).precio;
+            break;
+        default:
+            console.log('An error has occurred');
+    }
+    lineaCarrito.innerHTML = `${nombreProducto} x $${precioProducto}`;
+    padre.appendChild(lineaCarrito);
+    alert(`Has agregado 1 ${nombreProducto} al carrito`);
 }
